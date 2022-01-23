3df5ee06544eba7fae4eacda0b6368d1fccdf724
qing.ma <qing.ma@mosadex-ehealth.nl>
2022-01-23 09:31:17 +0100

    Add project files.

---
+++ WiredBrainCoffee.StorageApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using WiredBrainCoffee.StorageApp.Entities;
+using WiredBrainCoffee.StorageApp.Repositories;
+
+namespace WiredBrainCoffee.StorageApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var employeeRepository = new EmployeeRepository();
+            employeeRepository.Add(new Employee { FirstName = "Julia" });
+            employeeRepository.Add(new Employee { FirstName = "Anna" });
+            employeeRepository.Add(new Employee { FirstName = "Thomas" });
+            employeeRepository.Save();
+
+            Console.ReadLine();
+        }
+    }
+}
