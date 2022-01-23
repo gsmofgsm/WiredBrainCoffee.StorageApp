9058bd638ae88e31a4edf48b510b7137043252f6
qing.ma <qing.ma@mosadex-ehealth.nl>
2022-01-23 09:57:46 +0100

    Type Constraint

--- WiredBrainCoffee.StorageApp/Repositories/GenericRepository.cs
+++ WiredBrainCoffee.StorageApp/Repositories/GenericRepository.cs
@@ -1,15 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using WiredBrainCoffee.StorageApp.Entities;
 
 namespace WiredBrainCoffee.StorageApp.Repositories
 {
-    public class GenericRepository<T>
+    public class GenericRepository<T> where T : EntityBase
     {
         protected readonly List<T> _items = new();
 
+        public T GetById(int id)
+        {
+            return _items.Single(item => item.Id == id);
+        }
+
         public void Add(T item)
         {
+            item.Id = _items.Count + 1;
             _items.Add(item);
         }
         public void Remove(T item)
